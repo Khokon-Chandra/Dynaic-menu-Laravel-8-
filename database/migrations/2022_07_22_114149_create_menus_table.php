3f8508f1c64f8d742da1efc7ee1cddc3373944f4
Khokon Chandra <Khokonchandra4@gmail.com>
2022-07-22 17:49:39 +0600

    app component created

---
+++ database/migrations/2022_07_22_114149_create_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('parent_id')->nullable();
+            $table->string('title')->nullable();
+            $table->string('name');
+            $table->text('url')->nullable();
+            $table->integer('sequence')->nullable();
+            $table->boolean('is_newtab')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
